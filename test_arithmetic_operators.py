b906057aeb6a4208f320a1769338406eafc4d63a
Nez <nezzengame@gmail.com>
2023-09-17 22:04:03 +0200

    https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true

---
+++ test_arithmetic_operators.py
@@ -0,0 +1,6 @@
+from arithmetic_operators import addition, subtraction, multiplication
+
+def test_arithmetic_operators():
+    assert addition(3,2) == 5
+    assert subtraction(3,2) == 1
+    assert multiplication(3,2) == 6
