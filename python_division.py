69fa248618e7a82518a1bd71be69acc332d5c243
Nezno6 <76010886+Nezno6@users.noreply.github.com>
2023-09-17 23:31:11 +0200

    https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true (#24)

---
+++ python_division.py
@@ -0,0 +1,9 @@
+def integer_division(number_first,number_second):
+    return number_first//number_second
+
+def float_division(number_first,number_second):
+    return number_first/number_second
+
+def python_division(number_first,number_second):
+    integer_division(number_first,number_second)
+    float_division(number_first,number_second)
